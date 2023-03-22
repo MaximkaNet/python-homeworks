8044833e7b83359c82d880d3511398fe0c7ca404
Maksym <m.zavada2005@ukr.net>
2023-03-22 22:11:24 +0100

    rename methods

--- bot/database/methods/delete.py
+++ bot/database/methods/delete.py
@@ -4,15 +4,15 @@ from datetime import date
 
 
 class delete:
-    def delete_teacher(name: str):
+    def teacher(name: str):
         sql = f"DELETE FROM teachers WHERE name = '{name}'"
         query.delete(sql)
 
-    def delete_homework(date: date, author: str):
+    def homework(date: date, author: str):
         sql = "DELETE FROM homeworks WHERE date = %s AND author_id = (SELECT `id` FROM `teachers` WHERE `name` = %s)"
         val = (date.strftime(DATE_FORMAT), author)
         query.delete(sql, val)
 
-    def delete_tasks_by(homework_id: int):
+    def tasks_by(homework_id: int):
         sql = f"DELETE FROM `tasks` WHERE `homework_id` = {homework_id}"
         query.delete(sql)
