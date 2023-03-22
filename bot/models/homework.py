4aecc8d694f5a4bde3870365518ad37c74972765
Maksym <m.zavada2005@ukr.net>
2023-03-22 22:52:40 +0100

    Add new model: homework

---
+++ bot/models/homework.py
@@ -0,0 +1,63 @@
+from bot import models
+from bot.database.methods import update, insert, select, delete
+from bot.utils.env import Config
+
+from datetime import date
+import json
+
+
+class Homework:
+    def __init__(self, _date: date = None, _update: date = None, _author: str = "", _tasks: list = []):
+        self.date: date = _date
+        self.update_date: date = _date if _update == None else _update
+        self.author: str = _author
+        self.tasks: list[models.Task] = _tasks
+
+    def update(self, tasks: list):
+        self.tasks = tasks
+        self.update_date = date.today()
+        update.homework_by(self.author, self.date, self.update_date)
+        homework_id = select.homework_id(self.author, self.date)
+        delete.tasks_by(homework_id)
+        for task in self.tasks:
+            insert.task(task.source, json.dumps(
+                task.exercises), json.dumps(task.sentences), homework_id)
+
+    def create(self, author: str):
+        self.date = date.today()
+        self.update_date = date.today()
+        self.author = author
+        homework_id = insert.homework(
+            self.date, self.update_date, select.teacher_id(self.author))
+        for task in self.tasks:
+            insert.task(task.source, json.dumps(
+                task.exercises), json.dumps(task.sentences), homework_id)
+
+    # convert task list to unparsed type
+
+    def convert_tasks(self) -> str:
+        res = ""
+        for item in self.tasks:
+            res += f"\n{item.convert_unparsed()}"
+        return res
+
+    def parse_message(self, message: str) -> bool:
+        task_input = message.split("\n")
+        # validate message
+        for task_str in task_input:
+            if not models.Task.validate_message(task_str):
+                return False
+        # parse strings
+        self.tasks = []
+        for string in task_input:
+            temp = models.Task()
+            temp.string_parser(string)
+            self.tasks.append(temp)
+            temp = None
+        return True
+
+    def print(self, separator: str = "|-"):
+        sections: str = ""
+        for task in self.tasks:
+            sections += task.print(separator)
+        return f'*{self.author}*\n{sections}\n\n_Added: {self.date.strftime(Config.DATE_FORMAT)} ({self.date.strftime("%A")})_' if self.date == self.update_date else f'*{self.author}*\n{sections}\n\n_Updated: {self.update_date.strftime(Config.DATE_FORMAT)} ({self.date.strftime("%A")})_'
