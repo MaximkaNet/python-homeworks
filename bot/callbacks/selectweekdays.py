3062a1525a61e2fce3a1d4819b4ddfd634361034
Maksym <m.zavada2005@ukr.net>
2023-03-22 22:12:29 +0100

    Separate callbacks

---
+++ bot/callbacks/selectweekdays.py
@@ -0,0 +1,2 @@
+from aiogram.utils.callback_data import CallbackData
+week_days_callback = CallbackData("week_days", "act", "day")
---
+++ bot/callbacks/teacher.py
@@ -0,0 +1,4 @@
+from aiogram.utils.callback_data import CallbackData
+show_teacher_actions = CallbackData(
+    "teacher_show_actions", "act", "name", "work_days")
+choice_teacher_callback = CallbackData("teacher", "name")
