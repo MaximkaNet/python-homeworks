b98748aad24544bb07ade27541bc15f4c5aa7551
Maksym <m.zavada2005@ukr.net>
2023-03-24 20:20:29 +0100

    rename callback

--- bot/callbacks/homework.py
+++ bot/callbacks/homework.py
@@ -1,4 +1,4 @@
 from aiogram.utils.callback_data import CallbackData
 
 show_homework_callback = CallbackData("show_homework", "act", "src")
-showall_callback = CallbackData("all", "action", "author", "date")
+actions_show_all_callback = CallbackData("all", "action", "author", "date")
